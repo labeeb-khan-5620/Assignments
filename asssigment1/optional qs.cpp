56280b5bf45180997d30a8bf37601a3ba46db9af
labeeb-khan-5620 <labeeblabeeb562@gmail.com>
2025-10-11 04:58:23 -0700

    Add files via upload

---
+++ asssigment1/optional qs.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+//iostream library, necessary for input/output operations.
+using namespace std;
+int main()
+
+{
+int a,b;
+//declared two variables to store numbers.
+a=4;
+b=5;
+//stored values in variables.
+a=a+b;
+//we first added the numbers and stored it into first variable.
+b=a-b;
+// subtracted the second number from the the first variable and stored it into second variable.
+
+a=a-b;
+//subtraced the value stored in second variable from the first variable and stored it into first variable.
+
+cout << "value of first variable:"<<a<<endl;
+cout<<"value of second variable:"<<b;
+//for displaying the swapped values stored in the variables
+}
